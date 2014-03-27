e1794fc58e33a6a0f2fd02d026321d3ddf0a467d
François Boulogne <fboulogne sciunto org>
2014-03-26 20:01:23 -0400

    some corrections from Stéfan

--- skimage/figures/getting_started.py
+++ skimage/figures/getting_started.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 
 # Load picture
 from skimage import data
-img = data.coins()[0:95, 70:370]
+image = data.coins()[0:95, 70:370]
 
 plt.figure(figsize=(8, 5))
 plt.subplot(3, 2, 1)
-plt.imshow(img, cmap=plt.cm.gray)
+plt.imshow(image, cmap=plt.cm.gray)
 plt.title('Original', fontsize=30)
 plt.axis('off')
 
 # Histogram
-values, bins = np.histogram(img, bins=np.arange(256))
+values, bins = np.histogram(image, bins=np.arange(256))
 
 plt.subplot(3, 2, 2)
 plt.plot(bins[:-1], values)
@@ -24,20 +24,20 @@ plt.title('Histogram', fontsize=30)
 from skimage.filter import threshold_adaptive
 from skimage.morphology import closing, square
 
-bw = threshold_adaptive(img, 135)
+bw = threshold_adaptive(image, 95, offset=-15)
 
 plt.subplot(3, 2, 3)
 plt.imshow(bw, cmap=plt.cm.gray)
-plt.title('Threshold (=135)', fontsize=30)
+plt.title('Adaptive threshold', fontsize=30)
 plt.axis('off')
 
 # Find maxima
 from skimage.feature import peak_local_max
 
-coordinates = peak_local_max(img, min_distance=20)
+coordinates = peak_local_max(image, min_distance=20)
 
 plt.subplot(3, 2, 4)
-plt.imshow(img, cmap=plt.cm.gray)
+plt.imshow(image, cmap=plt.cm.gray)
 plt.autoscale(False)
 plt.plot([p[1] for p in coordinates], [p[0] for p in coordinates], 'r.')
 plt.axis('off')
@@ -46,7 +46,7 @@ plt.title('Peak local maxima', fontsize=30)
 # Detect edges
 from skimage import filter
 
-edges = filter.canny(img, sigma=3, low_threshold=10, high_threshold=80)
+edges = filter.canny(image, sigma=3, low_threshold=10, high_threshold=80)
 
 plt.subplot(3, 2, 5)
 plt.imshow(edges, cmap=plt.cm.gray)
@@ -61,7 +61,7 @@ from skimage.morphology import label
 label_image = label(edges)
 
 ax = plt.subplot(3, 2, 6)
-ax.imshow(img, cmap=plt.cm.gray)
+ax.imshow(image, cmap=plt.cm.gray)
 ax.set_title('Labeled items', fontsize=30)
 
 for region in regionprops(label_image):
