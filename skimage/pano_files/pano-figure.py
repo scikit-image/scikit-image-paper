ec59fe36dced3f407c9d6f62cf7285d908b62d71
Tony S Yu <tsyu80@gmail.com>
2014-03-31 23:07:59 -0500

    Rename variable for clarity

--- skimage/pano_files/pano-figure.py
+++ skimage/pano_files/pano-figure.py
@@ -9,32 +9,32 @@ plt.rcParams['text.color'] = 'gray'
 plt.rcParams['axes.edgecolor'] = 'lightgray'
 plt.rcParams['image.interpolation'] = 'nearest'
 fig = plt.figure(figsize=(8, 6))
-fig_shape = (5, 2)
+grid_shape = (5, 2)
 
 
 x0 = 0.01
 y0 = 0.99
 
-ax0 = plt.subplot2grid(fig_shape, (0, 0))
+ax0 = plt.subplot2grid(grid_shape, (0, 0))
 ax0.imshow(images[0])
 ax0.text(x0, y0, 'A', transform=ax0.transAxes, va='top', ha='left')
 
-ax1 = plt.subplot2grid(fig_shape, (1, 0), rowspan=2)
+ax1 = plt.subplot2grid(grid_shape, (1, 0), rowspan=2)
 ax1.imshow(images[1])
 ax1.text(x0, y0, 'B', transform=ax1.transAxes, va='top', ha='left')
 
-ax2 = plt.subplot2grid(fig_shape, (3, 0), rowspan=2)
+ax2 = plt.subplot2grid(grid_shape, (3, 0), rowspan=2)
 ax2.imshow(images[2])
 ax2.text(x0, y0, 'C', transform=ax2.transAxes, va='top', ha='left')
 
 x1 = 0.99
 y1 = 0.99
 
-ax3 = plt.subplot2grid(fig_shape, (0, 1))
+ax3 = plt.subplot2grid(grid_shape, (0, 1))
 ax3.imshow(images[3])
 ax3.text(x1, y1, 'D', transform=ax3.transAxes, va='top', ha='right')
 
-ax4 = plt.subplot2grid(fig_shape, (1, 1), rowspan=4)
+ax4 = plt.subplot2grid(grid_shape, (1, 1), rowspan=4)
 ax4.imshow(images[4])
 ax4.text(x1, y1, 'E', transform=ax4.transAxes, va='top', ha='right')
 
