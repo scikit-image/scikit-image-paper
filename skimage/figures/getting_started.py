d9ff36a654c214fffb5f8ab1064d1e6057a8b84e
Johannes Schönberger <jsch@demuc.de>
2014-05-18 14:55:42 -0400

    Improve figures

--- skimage/figures/getting_started.py
+++ skimage/figures/getting_started.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+
+
+# Setup figure
+matplotlib.rc('font', size=9)
+matplotlib.rc('xtick', labelsize=7)
+matplotlib.rc('ytick', labelsize=7)
+
+fig, axes = plt.subplots(ncols=2, nrows=3, figsize=(6, 4))
+fig.subplots_adjust(left=0.01, right=0.98, top=0.95, bottom=0.01,
+                    wspace=0.12, hspace=0.12)
+ax0, ax1, ax2, ax3, ax4, ax5  = axes.flat
+
 
 # Load picture.
 from skimage import data
 image = data.coins()[0:95, 70:370]
 
-fig, axes = plt.subplots(ncols=2, nrows=3, figsize=(6, 4.5))
-ax0, ax1, ax2, ax3, ax4, ax5  = axes.flat
 ax0.imshow(image, cmap=plt.cm.gray)
 ax0.set_title('Original')
 ax0.axis('off')
 
-# Histogram.
+
+# Histogram
 values, bins = np.histogram(image, bins=np.arange(256))
 
 ax1.plot(bins[:-1], values, lw=2, c='k')
@@ -21,7 +33,8 @@ ax1.set_yticks([0, 400])
 ax1.set_aspect(.2)
 ax1.set_title('Histogram')
 
-# Apply threshold.
+
+# Apply threshold
 from skimage.filter import threshold_adaptive
 
 bw = threshold_adaptive(image, 95, offset=-15)
@@ -30,7 +43,8 @@ ax2.imshow(bw, cmap=plt.cm.gray)
 ax2.set_title('Adaptive threshold')
 ax2.axis('off')
 
-# Find maxima.
+
+# Find maxima
 from skimage.feature import peak_local_max
 
 coordinates = peak_local_max(image, min_distance=20)
@@ -42,7 +56,8 @@ ax3.set_title('Peak local maxima')
 ax3.axis('off')
 ax3.axis('off')
 
-# Detect edges.
+
+# Detect edges
 from skimage import filter
 
 edges = filter.canny(image, sigma=3, low_threshold=10, high_threshold=80)
@@ -51,7 +66,8 @@ ax4.imshow(edges, cmap=plt.cm.gray)
 ax4.set_title('Edges')
 ax4.axis('off')
 
-# Label image regions.
+
+# Label image regions
 from skimage.measure import regionprops
 import matplotlib.patches as mpatches
 from skimage.morphology import label
@@ -63,12 +79,10 @@ ax5.set_title('Labeled items')
 ax5.axis('off')
 
 for region in regionprops(label_image):
-    # Draw rectangle around segmented coins.
+    # Draw rectangle around segmented coins
     minr, minc, maxr, maxc = region.bbox
     rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                               fill=False, edgecolor='red', linewidth=2)
     ax5.add_patch(rect)
 
-plt.tight_layout()
-plt.savefig('getting_started.png', dpi=200)
-plt.show()
+plt.savefig('getting_started.pdf', dpi=600)
